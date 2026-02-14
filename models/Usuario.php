3a54a713a654fcb99514be6682df645f58dbf547
Ruben <ruben@gmail.com>
2026-02-14 19:04:59 +0100

    hash de las contraseñas

--- models/Usuario.php
+++ models/Usuario.php
@@ -3,30 +3,25 @@ class Usuario {
     private $conn;
     private $table_name = "usuarios";
 
-    // Atributos según el esquema de la BD [cite: 14-17]
     public $id;
     public $username;
     public $password;
     public $rol;
 
     public function __construct($db){
-        $this->conn = $db; // Corregido: $this y punto y coma
+        $this->conn = $db; 
     }
 
-    public function login ($user, $pass){
-        // Corregido: Espacios en la cadena para que no se peguen las palabras
-        $consulta = "SELECT id, username, password, rol FROM " . 
-                    $this->table_name . " WHERE username = :username LIMIT 1";
-
-        $stmt = $this->conn->prepare($consulta); // Consulta preparada [cite: 64]
+    public function login($user, $pass) {
+        $consulta = "SELECT id, username, password, rol FROM " . $this->table_name . " WHERE username = :username LIMIT 1";
+        $stmt = $this->conn->prepare($consulta); 
         $stmt->bindParam(":username", $user);
         $stmt->execute();
 
         if($stmt->rowCount() > 0) {
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            if($pass == $row['password']){
-                // Corregido: Añadido $ a row y corregido 'true'
+        
+            if(password_verify($pass, $row['password'])){
                 $this->id = $row['id'];
                 $this->username = $row['username'];
                 $this->rol = $row['rol'];
@@ -36,16 +31,17 @@ class Usuario {
         return false;
     }
 
-    public function registrar($user, $pass) { // Corregido: nombre de variable $user
+    public function registrar($user, $pass) { 
         try {
-            // El rol 0 es para usuario normal según el enunciado [cite: 32]
+
+            $passHash = password_hash($pass, PASSWORD_DEFAULT);
+
             $query = "INSERT INTO " . $this->table_name . " (username, password, rol) VALUES (:user, :pass, 0)";
             $stmt = $this->conn->prepare($query);
             $stmt->bindParam(':user', $user);
-            $stmt->bindParam(':pass', $pass);
+            $stmt->bindParam(':pass', $passHash); 
             return $stmt->execute();
         } catch (PDOException $e) {
-            // Manejo de errores con excepciones [cite: 62]
             throw new Exception("Error al registrar: " . $e->getMessage()); 
         }
     }
